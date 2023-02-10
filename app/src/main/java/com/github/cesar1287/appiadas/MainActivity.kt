eb67a5efa86653549525f20a993ce0aa3a90950a
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-09 22:38:16 -0300

    finishing and answering exercise 1

--- app/src/main/java/com/github/cesar1287/appiadas/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/appiadas/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.github.cesar1287.appiadas
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.cesar1287.appiadas.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var lastRandomNumber = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun tellJoke() {
+        val jokes = resources.getStringArray(R.array.jokers)
 
+        val randomNumber = Random.nextInt(jokes.size)
+
+        if (lastRandomNumber != randomNumber) {
+            lastRandomNumber = randomNumber
+
+            val joke = jokes[randomNumber]
+            binding.tvJoke.text = joke
+            playSong()
+        } else {
+            tellJoke()
+        }
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
+        mediaPlayer.start()
     }
 }
